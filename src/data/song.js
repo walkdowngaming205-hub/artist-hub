d7d0cd880f199263f47431468dd61d7bfe396931
baby8 <walkdowngaming205@gmail.com>
2026-03-24 22:56:24 -0500

    Song data

---
+++ src/data/song.js
@@ -0,0 +1,15 @@
+const song =[
+    { id: 1, 
+      title: "Let Me Down",
+      artist: "Walk Deezy",
+    },
+    { id: 2, 
+      title: "Keep It 100",
+      artist: "Walk Deezy",
+    },
+    { id: 3, 
+      title: "Popular Loner",
+      artist: "Walk Deezy",
+    }
+
+];
